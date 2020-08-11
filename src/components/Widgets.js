3a42e8cedf7f45d2a08e8198cf81dbab5cb4dcb6
vaddv <vijay.vaddem@gmail.com>
2020-08-11 23:59:12 +0530

    No time for detail msg

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -16,6 +16,12 @@ function Widgets() {
       </div>
       <div className="widgets__widgetContainer">
         <h2>What's happening</h2>
+        <TwitterTweetEmbed></TwitterTweetEmbed>
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="cleverqazi"
+          options={{ height: 400 }}
+        />
       </div>
     </div>
   );
